8419244966e788d098556e2692a930056a495627
Miguel Gallegos <mgallegos>
2022-10-12 18:12:58 -0700

    Create binary-gap.php

---
+++ binary-gap.php
@@ -0,0 +1,39 @@
+<?php
+
+// React By Maximilian
+
+function solution($n)
+{
+    $str = decbin($n);
+
+    $startIdx = strpos($str, "1");
+    $endIdx = strrpos($str, "1");
+
+    if (false === $startIdx || false === $endIdx || 0 == $endIdx) {
+        return 0;
+    }
+
+    $str = str_replace('1', ',', $str);
+
+    $str = explode(',', $str);
+
+    $len = 0;
+    foreach ($str as $value) {
+        $len1 = strlen($value);
+        
+        if (0 == $len1) {
+            continue;
+        }
+
+        if ($len1 > $len) {
+            $len = $len1;
+        }
+    }
+
+    return $len;
+}
+
+foreach ([1, 2, 147, 483, 647] as $v) {
+    echo $v . ' -> ' . decbin($v) . ' -> ' . solution($v);
+    echo "\n";
+}
